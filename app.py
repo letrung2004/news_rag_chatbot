91ecff6a75635696edcc5851c64a6062eec25cdd
le trung <lequoctrunggg@gmail.com>
2025-08-20 23:26:06 +0700

    project packaging

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ def delete_article(articleId):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- configs/database.py
+++ configs/database.py
@@ -1,6 +1,9 @@
 from pymongo import MongoClient
+import os
 
-MONGO_URI = "mongodb://root:root@localhost:27017/ai-service?authSource=admin"
+# MONGO_URI = "mongodb://root:root@localhost:27017/ai-service?authSource=admin"
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb://root:root@mongodb:27017/ai-service?authSource=admin")
+
 
 client = MongoClient(MONGO_URI)
 
